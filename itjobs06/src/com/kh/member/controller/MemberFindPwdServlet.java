d17420e2608c37797fca8abd5c1166c5fc3586c3
gitteration <55491073+gitteration@users.noreply.github.com>
2019-10-16 21:34:31 +0900

    상화 - 비밀번호 암호화

--- itjobs06/src/com/kh/member/controller/MemberFindPwdServlet.java
+++ itjobs06/src/com/kh/member/controller/MemberFindPwdServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MemberFindPwdServlet
  */
-@WebServlet("/memfindpwd.me")
+@WebServlet(name="MemberFindPwdServlet",urlPatterns="/memfindpwd.me")
 public class MemberFindPwdServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
